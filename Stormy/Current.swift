ae8144026b45e0a200cf1776408831ce41d29f5d
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-02-05 02:39:53 -0800

    Finish app

--- Stormy/Current.swift
+++ Stormy/Current.swift
@@ -7,15 +7,25 @@
 //
 
 import Foundation
+import UIKit
 
 struct Current {
     
     var currentTime: String?
     var temperature: Int
+    var temperatureCelcius: Int {
+        return Int(Double(temperature - 32) / 1.8)
+    }
     var humidity: Double
+    var humidityRatio: Int {
+        return Int(humidity * 100)
+    }
     var precipProbability: Double
+    var precipProbabilityRatio: Int {
+        return Int(precipProbability * 100)
+    }
     var summary: String
-    var icon: String
+    var icon: UIImage?
     
     init(weatherDictionary: NSDictionary) {
         let currentWeather = weatherDictionary["currently"] as NSDictionary
@@ -24,10 +34,12 @@ struct Current {
         humidity = currentWeather["humidity"] as Double
         precipProbability = currentWeather["precipProbability"] as Double
         summary = currentWeather["summary"] as String
-        icon = currentWeather["icon"] as String
         
         let currentTimeIntValue = currentWeather["time"] as Int
         currentTime = dateStringFromUnixTime(currentTimeIntValue)
+        
+        let iconString = currentWeather["icon"] as String
+        icon = weatherIconFromString(iconString)
         
     }
     
@@ -39,6 +51,40 @@ struct Current {
         dateFormatter.timeZone = NSTimeZone(name: "America/Vancouver")
         
         return dateFormatter.stringFromDate(weatherDate)
+    }
+    
+    func weatherIconFromString(stringIcon: String) -> UIImage {
+        
+        var imageName: String
+        
+        switch stringIcon {
+        case "clear-day":
+            imageName = "clear-day"
+        case "clear-night":
+            imageName = "clear-night"
+        case "rain":
+            imageName = "rain"
+        case "snow":
+            imageName = "snow"
+        case "sleet":
+            imageName = "sleet"
+        case "wind":
+            imageName = "wind"
+        case "fog":
+            imageName = "fog"
+        case "cloudy":
+            imageName = "cloudy"
+        case "partly-cloudy-day":
+            imageName = "partly-cloudy"
+        case "partly-cloudy-night":
+            imageName = "cloudy-night"
+        default:
+            imageName = "default"
+        }
+        
+        return UIImage(named: imageName)!
+        
+        
     }
     
 }
--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var precipitationLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
+    
     private let apiKey = "4f9288af14b2c172d497f4538a8dfc80"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+    }
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forecastURL = NSURL(string: "49.275568,-123.127829", relativeToURL: baseURL)
         
@@ -24,16 +38,45 @@ class ViewController: UIViewController {
             if(error == nil) {
                 let dataObject = NSData(contentsOfURL: location)
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+                
                 let currentWeather = Current(weatherDictionary: weatherDictionary)
                 
-                println(currentWeather.currentTime!)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.temperatureLabel.text = "\(currentWeather.temperatureCelcius)"
+                    self.iconView.image = currentWeather.icon!
+                    self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
+                    self.humidityLabel.text = "\(currentWeather.humidityRatio)%"
+                    self.precipitationLabel.text = "\(currentWeather.precipProbabilityRatio)%"
+                    self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    // Stop refresh
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                    
+                })
                 
+            } else {
+                let networkIssueController = UIAlertController(title: "Network Error", message: "Unable to connect to the Internet", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // Stop refresh
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
             }
             
         })
         downloadTask.resume()
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,6 +84,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func refresh() {
+        
+        getCurrentWeatherData()
+        
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+    }
 
 }
 
