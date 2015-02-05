4f36e1ca2996532aa170f66a8558af6e1b04b84f
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-02-04 20:43:19 -0800

    Initial commit

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let apiKey = "4f9288af14b2c172d497f4538a8dfc80"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "49.275568,-123.127829", relativeToURL: baseURL)
     }
 
     override func didReceiveMemoryWarning() {
